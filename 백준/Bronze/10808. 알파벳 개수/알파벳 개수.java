23615c4fd2be4f984c68658ac395f36726517137
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2023-05-18 13:30:18 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 148 ms, Memory: 15836 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static String s;
+    private static final int ALPHABET_COUNT = 26;
+    private static char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                                      'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                      'o', 'p', 'q', 'r', 's', 't', 'u',
+                                      'v', 'w', 'x', 'y', 'z'};
+
+    public static void main(String[] args) throws IOException {
+        s = br.readLine();
+        int[] result = new int[ALPHABET_COUNT];
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < alphabet.length; j++) {
+                if (s.charAt(i) == alphabet[j]) {
+                    result[j] += 1;
+                }
+            }
+        }
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
