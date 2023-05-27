731d159a08709cceb84d3ef61da3c30182ecd859
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2023-05-27 15:39:11 +0900

    [Silver IV] Title: 큐 2, Time: 1584 ms, Memory: 407552 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+    private static final Queue<Integer> queue = new LinkedList<>();
+    private static Integer backNum;
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] command = br.readLine().split(" ");
+            switch (command[0]) {
+                case "push":
+                    queue.offer(Integer.parseInt(command[1]));
+                    backNum = Integer.parseInt(command[1]);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) sb.append(-1 + "\n");
+                    else sb.append(queue.poll() + "\n");
+                    break;
+                case "size":
+                    sb.append(queue.size() + "\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) sb.append(1 + "\n");
+                    else sb.append(0 + "\n");
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        sb.append(-1 + "\n");
+                    } else {
+                        sb.append(queue.peek() + "\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        sb.append(-1 + "\n");
+                    } else {
+                        sb.append(backNum + "\n");
+                    }
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+}
