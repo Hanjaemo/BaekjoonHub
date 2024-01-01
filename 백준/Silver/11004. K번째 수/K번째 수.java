fc36804d2482e0c416476f69067a51e534fe4446
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-01 23:19:51 +0900

    [Silver V] Title: K번째 수, Time: 1692 ms, Memory: 370292 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(arr[k - 1]);
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+        int part = partition(arr, start, end);
+        if (start < part - 1) {
+            quickSort(arr, start, part - 1);
+        }
+        if (part < end) {
+            quickSort(arr, part, end);
+        }
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];
+        while (start <= end) {
+            while (arr[start] < pivot) {
+                start++;
+            }
+            while (arr[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
