54e8a63e46f107b958824e86701a32f528dd6fec
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-07 01:16:27 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1672 ms, Memory: 128172 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -5,32 +5,40 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 표의 크기, 횟수 입력
+        // 2차원 배열의 크기 n, 구간 합 질의 개수 m
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        // 표의 크기 +1만큼 2차원 합 배열 생성
-        int[][] sumArr = new int[n + 1][n + 1];
+        // 배열 arr 생성
+        int[][] arr = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
-                sumArr[i][j] =
-                        sumArr[i][j - 1] + sumArr[i - 1][j] - sumArr[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // (x1,y1)와 (x2,y2)까지의 구간합
-        for (int q = 0; q < m; q++) {
+        // 합 배열 sumArr 생성
+        int[][] sumArr = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                sumArr[i][j] = sumArr[i - 1][j] + sumArr[i][j - 1] - sumArr[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        // for (i=0~m)
+            // 구간 합 계산
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int sum = sumArr[x2][y2] - sumArr[x1 - 1][y2] - sumArr[x2][y1 - 1] + sumArr[x1 - 1][y1 - 1];
-            System.out.println(sum);
+            int result = sumArr[x2][y2] - sumArr[x2][y1 - 1] - sumArr[x1 - 1][y2] + sumArr[x1 - 1][y1 - 1];
+            System.out.println(result);
         }
     }
 }
