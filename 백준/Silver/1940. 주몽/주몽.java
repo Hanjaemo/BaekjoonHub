263fe0540872bb7d9e42abc4a516214ea57bf3a3
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-07 01:54:52 +0900

    [Silver IV] Title: 주몽, Time: 204 ms, Memory: 16360 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -6,42 +6,49 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 재료 개수(n), 갑옷 개수(m) 입력
+        // 재료 개수 n 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
+        // 갑옷을 만드는 데 필요한 번호의 합 m 입력
         int m = Integer.parseInt(br.readLine());
 
-        // 배열 생성 및 정렬
+        // 크기가 n인 배열 arr 생성
         int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        // 배열 정렬
         Arrays.sort(arr);
 
-        // while (j < i)
-            // if (arr[i] + arr[j] < m) : i++
-            // if (arr[i] + arr[j] > m) : j--
-            // if (arr[i] + arr[j] == m) : count++, i++, j--
-        int i = 0;
-        int j = n - 1;
-        int count = 0;
-        while (i < j) {
-            int sum = arr[i] + arr[j];
+        // 시작점 s = 0
+        // 끝점 e = n-1
+        int s = 0;
+        int e = n - 1;
+
+        // cnt = 0
+        int cnt = 0;
+        // while (s <= e)
+            // if arr[s] + arr[e] < m : s++
+            // if arr[s] + arr[e] > m : e--
+            // if arr[s] + arr[e] == m : cnt++, s++, e--
+        while (s < e) {
+            int sum = arr[s] + arr[e];
             if (sum < m) {
-                i++;
-                continue;
+                s++;
             }
             if (sum > m) {
-                j--;
-                continue;
+                e--;
             }
-            count++;
-            i++;
-            j--;
+            if (sum == m) {
+                cnt++;
+                s++;
+                e--;
+            }
         }
 
-        // 만들 수 있는 갑옷 개수 출력
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
