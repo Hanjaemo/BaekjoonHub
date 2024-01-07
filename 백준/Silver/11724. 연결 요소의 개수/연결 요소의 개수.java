5bf680167b14f9e9607cacc09d8f64248bc3fcf8
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-07 15:22:59 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 724 ms, Memory: 141808 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,13 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    // 배열 arr
+    static List<List<Integer>> arr;
+    // 방문 배열 visited
+    static boolean[] visited;
+    // 연결 요소 개수 count
+    static int count;
+
     public static void main(String[] args) throws IOException {
         // 노드 개수 n, 에지 개수 m 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,52 +20,53 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        List<List<Integer>> arr = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) {
+        // 배열 arr, visited, count 초기화
+        arr = new ArrayList<>();
+        visited = new boolean[n + 1];
+        count = 0;
+
+        // 배열 arr에 인접리스트 저장
+        for (int i = 0; i <= n; i++) {
             arr.add(new ArrayList<>());
         }
-
-        // for (m번 반복)
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            // 입력 받은 에지의 양끝 점을 배열에 인접 리스트로 저장
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             arr.get(a).add(b);
             arr.get(b).add(a);
         }
 
-        Deque<Integer> deque = new ArrayDeque<>();
-        boolean[] visited = new boolean[n + 1];
-        int vertex;
-        int count = 0;
-        
+        // for (i=1~n)
+            // if 방문하지 않은 노드 : count++
+            // dfs 탐색
         for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
                 count++;
-                visited[i] = true;
-                if (deque.isEmpty()) {
-                    for (int j = 0; j < arr.get(i).size(); j++) {
-                        Integer adj = arr.get(i).get(j);
-                        if (!deque.contains(adj)) {
-                            deque.push(adj);
-                        }
-                    }
-                }
-                while (!deque.isEmpty()) {
-                    vertex = deque.pop();
-                    if (!visited[vertex]) {
-                        visited[vertex] = true;
-                        for (int j = 0; j < arr.get(vertex).size(); j++) {
-                            Integer adj = arr.get(vertex).get(j);
-                            if (!deque.contains(adj)) {
-                                deque.push(adj);
-                            }
-                        }
-                    }
-                }
+            }
+            dfs(i);
+        }
+
+        // 연결 요소 개수 출력
+        System.out.println(count);
+    }
+
+    private static void dfs(int v) {
+        // if 이미 방문한 노드 : 종료
+        if (visited[v]) {
+            return;
+        }
+
+        // 방문 처리
+        visited[v] = true;
+
+        // for (i=0~arr.get(v).size()-1)
+            // if 방문하지 않은 노드 : dfs 탐색
+        for (int i = 0; i < arr.get(v).size(); i++) {
+            int now = arr.get(v).get(i);
+            if (!visited[now]) {
+                dfs(now);
             }
         }
-        System.out.println(count);
     }
 }
