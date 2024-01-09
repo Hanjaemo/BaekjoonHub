102906fa9173117f9154cd5adb75c4d7a3d7c869
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-09 16:20:52 +0900

    [Bronze III] Title: 전자레인지, Time: 136 ms, Memory: 14356 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringJoiner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[] arr = {500, 60, 10};
+        int[] counts = new int[3];
+        while (t > 0) {
+            if (t % 10 != 0) {
+                System.out.println(-1);
+                return;
+            }
+            for (int i = 0; i < arr.length; i++) {
+                if (t >= arr[i]) {
+                    counts[i] = t / arr[i];
+                    t %= arr[i];
+                }
+            }
+        }
+        for (int count : counts) {
+            System.out.print(count + " ");
+        }
+    }
+}
