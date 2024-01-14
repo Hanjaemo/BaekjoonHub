2db84f2f91f08063e3f2d1712d331d358281b9aa
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-14 16:48:12 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -5,43 +5,37 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 동전 종류 수 n, 목표 금액 k 입력
+        // 동전 수 n, 목표 금액 k
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        // 각 동전의 개수를 저장하는 배열 coins 생성
+        // 동전의 종류 배열 coins
         int[] coins = new int[n];
         // for (n번 반복)
-            // 숫자를 입력 받아 coins에 저장
+            // coins에 입력 받은 동전 종류 저장
         for (int i = 0; i < n; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        // k를 위한 동전 개수 cnt = 0
-        int cnt = 0;
+        // 필요 동전 개수 count = 0
+        int count = 0;
+
         // while (k > 0)
-            // k보다 작거나 같은 동전 coin = 0
-            // for (coins 크기만큼 반복, 내림차순 탐색)
-                // if coins[i] <= k :
-                    // coin = coins[i]
-                    // 탈출
-            // cnt += k / coin
-            // k %= coin
+            // coins를 탐색하여 k보다 작거나 같은 값 선택
+            // count += k / 선택한 동전
+            // k %= 선택한 동전
         while (k > 0) {
-            int coin = 0;
             for (int i = coins.length - 1; i >= 0; i--) {
-                if (coins[i] <= k) {
-                    coin = coins[i];
+                if (k >= coins[i]) {
+                    count += k / coins[i];
+                    k %= coins[i];
                     break;
                 }
             }
-            cnt += k / coin;
-            k %= coin;
         }
 
-        // cnt 출력
-        System.out.println(cnt);
+        System.out.println(count);
     }
 }
