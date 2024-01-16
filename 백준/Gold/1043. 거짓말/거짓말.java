0c48ae6dfddf6ecc6fd3bc4f27f8330426cb5a7e
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-16 11:31:45 +0900

    [Gold IV] Title: 거짓말, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,22 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    // 대표 노드 저장 배열 arr
     static int[] arr;
-    // 진실 아는 사람 배열 knowPeople
-    static int[] knowPeople;
-    // 파티 참가자 배열 participants
-    static List<List<Integer>> participants = new ArrayList<>();
-    // 참가 가능한 파티 수 count = 0
-    static int count = 0;
 
     public static void main(String[] args) throws IOException {
-        // 사람 수 n, 파티 수 m 입력
+        // 사람 수 n, 파티 수 m
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
@@ -28,46 +19,45 @@ public class Main {
             arr[i] = i;
         }
 
-        // 진실을 아는 사람들의 번호를 입력 받아, knowPeople 배열에 저장
-        String[] inputKnowInfo = br.readLine().split(" ");
-        int knowPeopleCount = Integer.parseInt(inputKnowInfo[0]);
-        knowPeople = new int[knowPeopleCount];
+        // 진실을 아는 사람 배열 knowPeople
+        st = new StringTokenizer(br.readLine());
+        int knowPeopleCount = Integer.parseInt(st.nextToken());
+        int[] knowPeople = new int[knowPeopleCount];
         for (int i = 0; i < knowPeopleCount; i++) {
-            knowPeople[i] = Integer.parseInt(inputKnowInfo[i + 1]);
+            knowPeople[i] = Integer.parseInt(st.nextToken());
         }
 
-        // participants 초기화
-        for (int i = 0; i < m; i++) {
-            participants.add(new ArrayList<>());
-        }
-        
-        // for (m번 반복)
-            // i번째 파티에 참가하는 사람들의 번호를 participants에 인접 리스트로 저장
-        for (int i = 0; i < m; i++) {
-            String[] inputPartyInfo = br.readLine().split(" ");
-            int participantCount = Integer.parseInt(inputPartyInfo[0]);
-            for (int j = 1; j <= participantCount; j++) {
-                participants.get(i).add(Integer.parseInt(inputPartyInfo[j]));
-            }
+        // 진실을 아는 사람끼리 연결
+        for (int i = 1; i < knowPeopleCount; i++) {
+            union(knowPeople[0], knowPeople[i]);
         }
 
+        // 파티에 오는 사람 배열 participants
+        int[] participants = new int[m];
         // for (m번 반복)
-            // 각 파티의 첫 번째 참가자 firstParticipant
-            // 첫 번째 참가자와 i번째 파티 참가자들에 대한 union 연산
+            // 파티 정보 입력
+            // 각 파티별로 첫 사람과 나머지 사람들 간의 union 연산 수행
+            // 각 파티별로 첫 사람을 participants에 저장
         for (int i = 0; i < m; i++) {
-            int firstParticipant = participants.get(i).get(0);
-            for (int j = 1; j < participants.get(i).size(); j++) {
-                union(firstParticipant, participants.get(i).get(j));
+            st = new StringTokenizer(br.readLine());
+            int participantCount = Integer.parseInt(st.nextToken());
+            int firstParticipant = Integer.parseInt(st.nextToken());
+            if (participantCount > 1) {
+                for (int j = 1; j < participantCount; j++) {
+                    union(firstParticipant, Integer.parseInt(st.nextToken()));
+                }
             }
+            participants[i] = firstParticipant;
         }
-        
+
+        // 과장된 이야기 가능한 파티 수 count
+        int count = 0;
         // for (m번 반복)
-            // i번째 파티의 첫 번째 참가자가 진실을 아는 사람이라면 건너뛰고, 아니라면 count++
+            // knowPeople의 대표 노드에 find(participants[i])가 포함되지 않으면 count 1 증가
         for (int i = 0; i < m; i++) {
             boolean isPossible = true;
             for (int j = 0; j < knowPeopleCount; j++) {
-                int firstParticipants = participants.get(i).get(0);
-                if (find(firstParticipants) == find(knowPeople[j])) {
+                if (find(participants[i]) == find(knowPeople[j])){
                     isPossible = false;
                     break;
                 }
@@ -82,7 +72,7 @@ public class Main {
     }
 
     private static int getParent(int node) {
-        if (arr[node] == node) {
+        if (node == arr[node]) {
             return node;
         }
         return arr[node] = getParent(arr[node]);
@@ -91,6 +81,7 @@ public class Main {
     private static void union(int a, int b) {
         a = getParent(a);
         b = getParent(b);
+
         if (a < b) {
             arr[b] = a;
         } else {
