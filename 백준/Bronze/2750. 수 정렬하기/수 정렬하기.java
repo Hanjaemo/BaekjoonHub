9b1a36b3e1b213195a8c685686f9958a8cb59b3e
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-29 13:17:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 184 ms, Memory: 14864 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,38 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 수의 개수 n 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()); // 배열의 크기
 
-        // n번 반복하여 숫자를 입력 받아 배열 생성
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        // 배열 생성 및 초기화
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        // 오름차순 정렬 (버블 정렬 사용)
-        for (int i = 0; i < n; i++) {
-            boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
+        bubbleSort(arr);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 1; i <= arr.length; i++) { // 정렬할 루프 범위 설정
+            boolean isSwap = false; // 루프 안에서 swap 연산이 수행되었는지 판단하는 변수
+            for (int j = 1; j <= arr.length - 1 - i; j++) { // 서로 인접한 두 데이터의 크기를 비교해 swap 연산 수행
                 if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                    swapped = true;
+                    swap(arr, j, j + 1);
+                    isSwap = true; // swap 연산이 수행되었음
                 }
             }
-            if (!swapped) {
-                break;
+            if (!isSwap) { // swap 연산이 한 번도 수행되지 않은 경우 정렬 종료
+                return;
             }
         }
+    }
 
-        // 결과 출력
-        for (int i : arr) {
-            System.out.println(i);
-        }
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
