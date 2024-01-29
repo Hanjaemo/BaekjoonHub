ed3f93396b01103bdd75dfe4f62f944b26d991f2
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-01-29 17:37:00 +0900

    [Bronze III] Title: 세 수, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        selectionSort(arr);
+
+        System.out.println(arr[1]);
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = Integer.MAX_VALUE;
+            int minIndex = 0;
+            for (int j = i; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int minIndex) {
+        int tmp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = tmp;
+    }
+}
