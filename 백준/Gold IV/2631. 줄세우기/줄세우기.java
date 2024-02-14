21bcaf324a08e2a6989f6544c708c48db43691be
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-02-15 02:04:20 +0900

    [Gold IV] Title: 줄세우기, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Gold IV/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        Arrays.sort(dp);
+        System.out.println(n - dp[n - 1]);
+    }
+}
