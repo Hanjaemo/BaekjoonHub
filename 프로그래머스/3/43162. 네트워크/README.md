55885d5df1ec9ade0d3bd2c7715c63bdcd3cf24b
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-02-24 21:31:51 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#qna) 
 
 ### 성능 요약
 
-메모리: 92.9 MB, 시간: 1.37 ms
+메모리: 74.2 MB, 시간: 0.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 24일 19:54:27
+2024년 02월 24일 21:31:49
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,55 +1,52 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
-    static List<List<Integer>> graph = new ArrayList<>();
-    static int answer = 0;
+    static int[] arr;
     
     public int solution(int n, int[][] computers) {
-        visited = new boolean[computers.length];
-        
-        for (int i=0;i<computers.length;i++) {
-            for (int j=0;j<computers[i].length;j++) {
-                graph.add(new ArrayList<>());
-            }
+        arr = new int[n];
+        for (int i=0;i<n;i++) {
+            arr[i] = i;
         }
         
         for (int i=0;i<computers.length;i++) {
             for (int j=0;j<computers[i].length;j++) {
+                if (i == j) {
+                    continue;
+                }
                 if (computers[i][j] == 1) {
-                    graph.get(i).add(j);
+                    union(i, j);
                 }
             }
         }
-        
-        for (int i=0;i<computers.length;i++) {
-            bfs(i);
+                
+        Set<Integer> set = new HashSet<>();
+        for (int i=0;i<n;i++) {
+            set.add(find(arr[i]));
         }
         
-        return answer;
+        return set.size();
     }
     
-    // bfs
-    public void bfs(int node) {
-        if (visited[node]) {
+    public int find(int node) {
+        if (node == arr[node]) {
+            return node;
+        }
+        return arr[node] = find(arr[node]);
+    }
+    
+    public void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        
+        if (a == b) {
             return;
         }
         
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(node);
-        visited[node] = true;
-        
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-            for (int i=0;i<graph.get(now).size();i++) {
-                int adj = graph.get(now).get(i);
-                if (!visited[adj]) {
-                    visited[adj] = true;
-                    queue.add(adj);
-                }
-            }
+        if (a < b) {
+            arr[b] = a;
+        } else {
+            arr[a] = b;
         }
-        
-        answer++;
     }
 }
