b9ec4c2316bfd40b2ef4b0f5528b26e95993a72c
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-02-28 23:34:21 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,3 +1,4 @@
-select floor(price/10000)*10000 as price_group, count(*) as products from product
+select floor(price/10000)*10000 as price_group, count(*) as products
+from product
 group by price_group
 order by price_group
