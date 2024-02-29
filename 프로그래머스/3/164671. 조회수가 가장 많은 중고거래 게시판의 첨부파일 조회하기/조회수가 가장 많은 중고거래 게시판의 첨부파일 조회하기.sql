c69ee81bf2eb7a184833b4bd78de9ba6df39c99c
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-02-29 20:19:42 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,6 +1,5 @@
-select concat('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) as file_path from used_goods_file
-where board_id = (
-    select board_id from used_goods_board 
-    order by views desc 
-    limit 1)
-order by file_id desc
+
+select concat('/home/grep/src/', b.board_id, '/', f.file_id, f.file_name, f.file_ext) as file_path from used_goods_board as b
+join used_goods_file as f using(board_id)
+where b.views = (select max(views) from used_goods_board)
+order by f.file_id desc
