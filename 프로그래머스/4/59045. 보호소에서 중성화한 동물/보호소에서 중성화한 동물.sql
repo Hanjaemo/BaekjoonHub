8e99d44c4b13a3aeed04f3ff5376ce552233002f
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-02-29 18:48:23 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,3 +1,4 @@
-select o.animal_id, o.animal_type, o.name from animal_outs as o
-join animal_ins as i on o.animal_id = i.animal_id
+select i.animal_id, i.animal_type, i.name from animal_ins as i
+join animal_outs as o using(animal_id)
 where i.sex_upon_intake like 'Intact%' and (o.sex_upon_outcome like 'Spayed%' or o.sex_upon_outcome like 'Neutered%')
+order by i.animal_id
