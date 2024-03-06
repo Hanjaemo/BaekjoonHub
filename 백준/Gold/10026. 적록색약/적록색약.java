3c2f31acfad4b6c4b5774e6766775316b05f51b9
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-03-06 11:39:55 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 15176 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,95 +3,85 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int n;
-    private static String[][] normalMap, blindMap;
-    private static StringTokenizer st;
-    private static final Queue<Pair> queue = new LinkedList<>();
-    private static final int[] dx = {1, 0, -1, 0};
-    private static final int[] dy = {0, 1, 0, -1};
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dRow = {1, 0, -1, 0};
+    static int[] dCol = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
-        inputMapSize();
-        setMap();
-        int normalAnswer = bfs(normalMap);
-        int blindAnswer = bfs(blindMap);
-        System.out.print(normalAnswer + " " + blindAnswer);
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    private static void inputMapSize() throws IOException {
-        n = Integer.parseInt(br.readLine());
-    }
-
-    private static void setMap() throws IOException {
-        normalMap = new String[n][n];
-        blindMap = new String[n][n];
+        map = new char[n][n];
         for (int i = 0; i < n; i++) {
-            String[] split = br.readLine().split("");
+            String input = br.readLine();
             for (int j = 0; j < n; j++) {
-                if (split[j].equals("G")) {
-                    blindMap[i][j] = "R";
-                } else {
-                    blindMap[i][j] = split[j];
-                }
-                normalMap[i][j] = split[j];
+                map[i][j] = input.charAt(j);
             }
         }
+
+        StringBuilder sb = new StringBuilder();
+
+        visited = new boolean[n][n];
+        sb.append(countArea(n)).append(" ");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 'R') {
+                    map[i][j] = 'G';
+                }
+            }
+        }
+
+        visited = new boolean[n][n];
+        sb.append(countArea(n));
+
+        System.out.println(sb);
     }
 
-    private static int bfs(String[][] map) {
-        boolean[][] visit = new boolean[n][n];
+    private static int countArea(int n) {
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (visit[i][j]) {
-                    continue;
-                }
-                queue.offer(new Pair(i, j));
-                visit[i][j] = true;
-                count++;
-                while (!queue.isEmpty()) {
-                    Pair p = queue.poll();
-                    for (int k = 0; k < 4; k++) {
-                        int nx = p.getX() + dx[k];
-                        int ny = p.getY() + dy[k];
-                        if (outOfRange(nx, ny)) {
-                            continue;
-                        }
-                        if (map[nx][ny].equals(map[p.getX()][p.getY()]) && !visit[nx][ny]) {
-                            queue.offer(new Pair(nx, ny));
-                            visit[nx][ny] = true;
-                        }
-                    }
+                if (!visited[i][j]) {
+                    bfs(i, j);
+                    count++;
                 }
             }
         }
+
         return count;
     }
 
-    private static boolean outOfRange(int nx, int ny) {
-        return (nx < 0 || nx >= n || ny < 0 || ny >= n);
+    public static void bfs(int i, int j) {
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(i, j));
+        visited[i][j] = true;
+
+        while (!queue.isEmpty()) {
+            Pair now = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int nr = now.r + dRow[d];
+                int nc = now.c + dCol[d];
+                if (nr < 0 || nr >= map.length || nc < 0 || nc >= map.length) {
+                    continue;
+                }
+                if (!visited[nr][nc] && map[now.r][now.c] == map[nr][nc]) {
+                    visited[nr][nc] = true;
+                    queue.add(new Pair(nr, nc));
+                }
+            }
+        }
     }
 }
 
 class Pair {
-    private int x;
-    private int y;
+    int r, c;
 
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
+    public Pair(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
 }
