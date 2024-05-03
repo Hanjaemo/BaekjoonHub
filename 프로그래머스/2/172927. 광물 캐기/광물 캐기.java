221c6909e2fff909ee68e429903121750c5411a8
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-05-03 10:55:03 +0900

    [level 2] Title: 광물 캐기, Time: 0.91 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] picks, String[] minerals) {
+        // 캘 수 있는 최대 광물 개수
         int maxMinerals = Math.min(picks[0] * 5 + picks[1] * 5 + picks[2] * 5, minerals.length);
         
+        // 광물들을 5개씩 분리 및 각 chunk별로 dia > iron > stone 순으로 정렬
         Queue<Chunk> chunks = new PriorityQueue<>(Collections.reverseOrder());
         for (int i=0;i<maxMinerals;i+=5) {
             String[] copied = Arrays.copyOfRange(minerals, i, Math.min(i+5, maxMinerals));
@@ -22,26 +24,29 @@ class Solution {
             chunks.add(chunk);
         }
         
+        // 각 chunk별로 피로도 구하기
         int answer = 0;
         for (int i=0;i<3;i++) {
             if (picks[i] < 1) {
                 continue;
             }
+            
             if (chunks.isEmpty()) {
                 break;
             }
+            
             for (int j=0;j<picks[i];j++) {
                 if (chunks.isEmpty()) {
                     break;
                 }
                 Chunk now = chunks.poll();
-                if (i == 0) {
+                if (i == 0) { // 다이아몬드 곡괭이
                     answer += now.dia + now.iron + now.stone;
                 }
-                if (i == 1) {
+                if (i == 1) { // 철 곡괭이
                     answer += now.dia*5 + now.iron + now.stone;
                 }
-                if (i == 2) {
+                if (i == 2) { // 돌 곡괭이
                     answer += now.dia*25 + now.iron*5 + now.stone;
                 }
             }
