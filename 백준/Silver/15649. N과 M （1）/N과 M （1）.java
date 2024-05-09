4cb5e3eddcf3d1dddb72db676cdbdfc7f70037fd
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-05-09 20:53:38 +0900

    [Silver III] Title: N과 M (1), Time: 2872 ms, Memory: 74412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        dfs(n, m, 0, new ArrayList<>(), new boolean[n + 1]);
+    }
+
+    public static void dfs(int n, int m, int depth, List<Integer> list, boolean[] visited) {
+        if (depth == m) {
+            for (int i : list) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            list.add(i);
+            visited[i] = true;
+            dfs(n, m, depth + 1, list, visited);
+            list.remove(list.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
