e86d2e369b8aed94a6b3bbd141cea1c92ac18c87
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-05-13 09:43:42 +0900

    [level 2] Title: 배달, Time: 1.42 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 1.04 ms
+메모리: 76.2 MB, 시간: 1.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 01일 00:49:46
+2024년 05월 13일 09:43:39
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,54 +2,57 @@ import java.util.*;
 
 class Solution {
     static List<List<Edge>> graph = new ArrayList<>();
-    static int[] distance;
     
-    public int solution(int n, int[][] road, int k) {
-        for (int i=0;i<=n;i++) {
+    public int solution(int N, int[][] road, int K) {
+        for (int i=0;i<=N;i++) {
             graph.add(new ArrayList<>());
         }
         for (int i=0;i<road.length;i++) {
-            int a = road[i][0];
-            int b = road[i][1];
+            int s = road[i][0];
+            int e = road[i][1];
             int d = road[i][2];
-            graph.get(a).add(new Edge(b, d));
-            graph.get(b).add(new Edge(a, d));
+            graph.get(s).add(new Edge(e, d));
+            graph.get(e).add(new Edge(s, d));
         }
         
-        for (List<Edge> list : graph) {
-            Collections.sort(list);
+        int[] distance = new int[N+1];
+        for (int i=2;i<=N;i++) {
+            distance[i] = Integer.MAX_VALUE;
         }
         
-        dijkstra(n, 1);
-        
-        int answer = 0;
-        for (int i=1;i<=n;i++) {
-            if (distance[i] <= k) {
-                answer++;
-            }
+        for (int i=1;i<=N;i++) {
+            Collections.sort(graph.get(i));
         }
         
-        return answer;
+        dijkstra(1, distance);
+        return answer(distance, K);
     }
     
-    public void dijkstra(int n, int start) {
-        distance = new int[n+1];
-        Arrays.fill(distance, Integer.MAX_VALUE);
-        distance[start] = 0;
+    public void dijkstra(int startNode, int[] distance) {
+        Queue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(startNode, 0));
         
-        Queue<Edge> queue = new PriorityQueue<>();
-        queue.add(new Edge(start, 0));
-        
-        while (!queue.isEmpty()) {
-            Edge now = queue.poll();
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
             for (int i=0;i<graph.get(now.n).size();i++) {
                 Edge adj = graph.get(now.n).get(i);
                 if (distance[adj.n] > distance[now.n] + adj.d) {
                     distance[adj.n] = distance[now.n] + adj.d;
-                    queue.add(new Edge(adj.n, distance[adj.n]));
+                    pq.add(new Edge(adj.n, distance[adj.n]));
                 }
             }
         }
+    }
+    
+    public int answer(int[] distance, int K) {
+        int cnt = 0;
+        for (int i=1;i<distance.length;i++) {
+            if (distance[i] <= K) {
+                cnt++;
+            }
+        }
+        
+        return cnt;
     }
 }
 
