6480a92de57430f06af88ff34015ff58208d3235
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-06-13 09:53:11 +0900

    [Gold V] Title: 빗물, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -5,13 +5,6 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-/**
- * 2 + 1 + 1 +1 + 1
- * 1 3 1 2 3 2 4 3 3 2
- *
- * if (max > arr[i]) push(i)
- * else -> 모두 pop 하면서 answer += max - arr[i]
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,38 +12,42 @@ public class Main {
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
         int[] arr = new int[w];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < w; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int answer = 0;
-
         Deque<Integer> stack = new ArrayDeque<>();
-        int max = arr[0];
+
+        // 왼 -> 오
+        int base = arr[0];
         for (int i = 1; i < w; i++) {
-            if (arr[i] < max) {
-                stack.push(i);
+            if (base >= arr[i]) {
+                stack.push(arr[i]);
             } else {
                 while (!stack.isEmpty()) {
-                    answer += max - arr[stack.pop()];
+                    int popped = stack.pop();
+                    answer += base - popped;
                 }
-                stack.push(i);
-                max = arr[i];
+                base = arr[i];
             }
         }
 
         stack.clear();
-        max = arr[w - 1];
-        for (int i = w - 1; i >= 0; i--) {
-            if (arr[i] <= max) {
-                stack.push(i);
+
+        // 오 -> 왼
+        base = arr[w - 1];
+        for (int i = w-2; i >= 0; i--) {
+            if (base > arr[i]) {
+                stack.push(arr[i]);
             } else {
                 while (!stack.isEmpty()) {
-                    answer += max - arr[stack.pop()];
+                    int popped = stack.pop();
+                    answer += base - popped;
                 }
-                stack.push(i);
-                max = arr[i];
+                base = arr[i];
             }
         }
 
