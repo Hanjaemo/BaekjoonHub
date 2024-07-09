54427da665b323a1254105927f6aaf65b6b56820
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-09 23:18:53 +0900

    [Gold V] Title: 숫자고르기, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,51 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static boolean[] visited;
-    static int to = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        arr = new int[n + 1];
+        int[] arr = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            int to = Integer.parseInt(br.readLine());
-            arr[i] = to;
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        visited = new boolean[n + 1];
-        List<Integer> result = new ArrayList<>();
+        Set<Integer> set = new TreeSet<>();
         for (int i = 1; i <= n; i++) {
-            dfs(i);
-            if (i == to) {
-                result.add(i);
+            if (i == dfs(arr, new boolean[n + 1], i)) {
+                set.add(i);
             }
-            to = 0;
-            visited = new boolean[n + 1];
         }
 
-        System.out.println(result.size());
-        Collections.sort(result);
-        for (Integer integer : result) {
+        List<Integer> toList = new ArrayList<>(set);
+        Collections.sort(toList);
+        System.out.println(toList.size());
+        for (Integer integer : toList) {
             System.out.println(integer);
         }
     }
 
-    public static void dfs(int node) {
-        if (visited[node]) {
-            to = node;
-            return;
+    public static int dfs(int[] arr, boolean[] visited, int idx) {
+        if (visited[idx]) {
+            return idx;
         }
-
-        visited[node] = true;
-        to = node;
-        dfs(arr[node]);
+        visited[idx] = true;
+        return dfs(arr, visited, arr[idx]);
     }
 }
