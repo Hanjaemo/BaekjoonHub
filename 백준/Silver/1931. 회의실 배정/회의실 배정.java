7286888fb45cf7bd2ea7c4b0a46707ea2f4c53b8
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-12 01:45:50 +0900

    [Silver I] Title: 회의실 배정, Time: 412 ms, Memory: 42072 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,41 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        int[][] arr = new int[n][2];
+        Queue<Meeting> pq = new PriorityQueue<>();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            pq.add(new Meeting(s, e));
+        }
+
+        int lastEnd = -1;
+        int answer = 0;
+        while (!pq.isEmpty()) {
+            Meeting now = pq.poll();
+            if (now.s >= lastEnd) {
+                answer++;
+                lastEnd = now.e;
             }
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) {
-                    return o1[0] - o2[0];
-                }
-                return o1[1] - o2[1];
-            }
-        });
+        System.out.println(answer);
+    }
+}
 
-        int count = 0;
-        int end = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i][0] >= end) {
-                end = arr[i][1];
-                count++;
-            }
+class Meeting implements Comparable<Meeting> {
+    int s, e;
+
+    public Meeting(int s, int e) {
+        this.s = s;
+        this.e = e;
+    }
+
+    @Override
+    public int compareTo(Meeting o) {
+        if (this.e == o.e) {
+            return this.s - o.s;
         }
-        System.out.println(count);
+        return this.e - o.e;
     }
 }
