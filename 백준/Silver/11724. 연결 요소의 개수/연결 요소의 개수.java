f2022b91d0077b1a41bd756126a65a93d6edcf5b
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-01-21 11:25:23 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 588 ms, Memory: 142212 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,71 +2,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    // 배열 arr
-    static List<List<Integer>> arr;
-    // 방문 배열 visited
-    static boolean[] visited;
-    // 연결 요소 개수 count
-    static int count;
-
     public static void main(String[] args) throws IOException {
-        // 노드 개수 n, 에지 개수 m 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        // 배열 arr, visited, count 초기화
-        arr = new ArrayList<>();
-        visited = new boolean[n + 1];
-        count = 0;
-
-        // 배열 arr에 인접리스트 저장
+        List<List<Integer>> edges = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
-            arr.add(new ArrayList<>());
+            edges.add(new ArrayList<>());
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            arr.get(a).add(b);
-            arr.get(b).add(a);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            edges.get(u).add(v);
+            edges.get(v).add(u);
         }
 
-        // for (i=1~n)
-            // if 방문하지 않은 노드 : count++
-            // dfs 탐색
+        int answer = 0;
+        boolean[] visited = new boolean[n + 1];
         for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                count++;
+            if (visited[i]) {
+                continue;
             }
-            dfs(i);
+            bfs(edges, visited, i);
+            answer++;
         }
-
-        // 연결 요소 개수 출력
-        System.out.println(count);
+        System.out.println(answer);
     }
 
-    private static void dfs(int v) {
-        // if 이미 방문한 노드 : 종료
-        if (visited[v]) {
-            return;
-        }
+    public static void bfs(List<List<Integer>> edges, boolean[] visited, int startNode) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startNode);
+        visited[startNode] = true;
 
-        // 방문 처리
-        visited[v] = true;
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+            for (int i = 0; i < edges.get(now).size(); i++) {
+                int adj = edges.get(now).get(i);
+                if (!visited[adj]) {
+                    queue.add(adj);
+                    visited[adj] = true;
 
-        // for (i=0~arr.get(v).size()-1)
-            // if 방문하지 않은 노드 : dfs 탐색
-        for (int i = 0; i < arr.get(v).size(); i++) {
-            int now = arr.get(v).get(i);
-            if (!visited[now]) {
-                dfs(now);
+                }
             }
         }
+    }
+}
+
+class Edge {
+    int u, v;
+
+    public Edge(int u, int v) {
+        this.u = u;
+        this.v = v;
     }
 }
