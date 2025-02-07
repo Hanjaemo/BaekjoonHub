28d086d70d6d5534a0f61ac04ae172b18c7dfde5
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-02-08 00:22:33 +0900

    [Silver I] Title: 회전 초밥, Time: 2284 ms, Memory: 303980 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] belt = new int[n];
+        for (int i = 0; i < n; i++) {
+            belt[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int s = 0; s < n; s++) {
+            int e = (s + k - 1) % n;
+            set.clear();
+
+            if (s > e) {
+                for (int i = s; i < n; i++) {
+                    set.add(belt[i]);
+                }
+                for (int i = 0; i <= e; i++) {
+                    set.add(belt[i]);
+                }
+            } else {
+                for (int i = s; i <= e; i++) {
+                    set.add(belt[i]);
+                }
+            }
+            set.add(c);
+            if (set.size() > max) {
+                max = set.size();
+            }
+        }
+
+        System.out.println(max);
+    }
+}
