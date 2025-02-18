0639be5ed99a70b49b0393287ac8ebefd7070fc4
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-02-18 11:44:20 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 28.41 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,71 +1,20 @@
-import java.util.*;
 class Solution {
     public long solution(int[] sequence) {
-        int size = sequence.length;
+        int n = sequence.length;
+        long[][] dp = new long[2][n];
+        dp[0][0] = sequence[0];
+        dp[1][0] = -sequence[0];
         
-        // 펄스 수열 생성
-        int[] p1 = makePulse(1, size);
-        int[] p2 = makePulse(-1, size);
-        
-        // 펄스 곱
-        int[] prod1 = pulseProduct(p1, sequence);
-        int[] prod2 = pulseProduct(p2, sequence);
-        
-        // 누적합
-        long[] sumProd1 = makeSumArr(prod1);
-        long[] sumProd2 = makeSumArr(prod2);
-        
-        // 부분합
-        long[] partSumProd1 = makePartSumArr(sumProd1);
-        long[] partSumProd2 = makePartSumArr(sumProd2);
-        
-        // 최댓값
-        long answer = -100000;
-        for (long i : partSumProd1) {
-            answer = Math.max(answer, i);
+        for (int i=1;i<n;i++) {
+            dp[0][i] = Math.max(dp[1][i-1], 0) + sequence[i];
+            dp[1][i] = Math.max(dp[0][i-1], 0) - sequence[i];
         }
         
-        for (long i : partSumProd2) {
-            answer = Math.max(answer, i);
+        long answer = 0;
+        for (int i=0;i<n;i++) {
+            answer = Math.max(answer, dp[0][i]);
+            answer = Math.max(answer, dp[1][i]);
         }
-        
         return answer;
-    }
-    
-    public int[] makePulse(int startSign, int size) {
-        int[] pulseArr = new int[size];
-        for (int i=0;i<size;i++) {
-            pulseArr[i] = startSign;
-            startSign *= -1;
-        }
-        return pulseArr;
-    }
-    
-    public int[] pulseProduct(int[] arr, int[] pulseArr) {
-        int[] productArr = new int[arr.length];
-        for (int i=0;i<arr.length;i++) {
-            productArr[i] = arr[i] * pulseArr[i];
-        }
-        return productArr;
-    }
-    
-    public long[] makeSumArr(int[] arr) {
-        long[] sumArr = new long[arr.length];
-        sumArr[0] = arr[0];
-        for (int i=1;i<arr.length;i++) {
-            sumArr[i] = sumArr[i-1] + arr[i];
-        }
-        return sumArr;
-    }
-    
-    public long[] makePartSumArr(long[] sumProdArr) {
-        long[] dp = new long[sumProdArr.length];
-        dp[0] = sumProdArr[0];
-        long min = Math.min(0, dp[0]);
-        for (int i=1;i<sumProdArr.length;i++) {
-            dp[i] = Math.max(sumProdArr[i] - min, sumProdArr[i]);
-            min = Math.min(min, sumProdArr[i]);
-        }
-        return dp;
     }
 }
