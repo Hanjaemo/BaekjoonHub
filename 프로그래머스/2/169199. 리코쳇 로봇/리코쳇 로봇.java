98efb9f38c688e3543ad648005241c402e0457ec
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-05-31 01:35:41 +0900

    [level 2] Title: 리코쳇 로봇, Time: 12.46 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,85 +1,68 @@
 import java.util.*;
 
 class Solution {
-    static String[][] map;
-    static int[][] distMap;
     static int[] dRow = {1,0,-1,0};
     static int[] dCol = {0,1,0,-1};
     
     public int solution(String[] board) {
-        map = new String[board.length][board[0].length()];
-        distMap = new int[board.length][board[0].length()];
-        
-        for (int i=0;i<board.length;i++) {
-            String[] split = board[i].split("");
-            for (int j=0;j<split.length;j++) {
-                map[i][j] = split[j];
-            }
+        int[][] dist = new int[board.length][board[0].length()];
+        for (int i=0;i<dist.length;i++) {
+            Arrays.fill(dist[i], -1);
         }
         
-        int answer = 0;
-        for (int i=0;i<map.length;i++) {
-            for (int j=0;j<map[i].length;j++) {
-                if (map[i][j].equals("R")) {
-                    answer = bfs(i,j);
-                    break;
+        Point start = null;
+        Point end = null;
+        for (int i=0;i<dist.length;i++) {
+            for (int j=0;j<dist[i].length;j++) {
+                if (board[i].charAt(j) == 'R') {
+                    start = new Point(i, j);
+                }
+                if (board[i].charAt(j) == 'G') {
+                    end = new Point(i, j);
                 }
             }
         }
         
-        return answer;
+        bfs(dist, board, start, end);
+        for (int i=0;i<dist.length;i++) {
+            System.out.println(Arrays.toString(dist[i]));
+        }
+        
+        return dist[end.r][end.c];
     }
     
-    public int bfs(int i, int j) {
-        Queue<Pair> queue = new LinkedList<>();
-        queue.add(new Pair(i, j));
+    public void bfs(int[][] dist, String[] board, Point start, Point end) {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(start);
+        dist[start.r][start.c] = 0;
         
         while (!queue.isEmpty()) {
-            Pair now = queue.poll();
-            if (map[now.r][now.c].equals("G")) {
-                return distMap[now.r][now.c];
-            }
+            Point now = queue.poll();
             for (int d=0;d<4;d++) {
                 int nr = now.r;
                 int nc = now.c;
                 while (true) {
-                    if (isOut(nr, nc) || map[nr][nc].equals("D")) {
+                    nr += dRow[d];
+                    nc += dCol[d];
+                    if (nr < 0 || nr >= dist.length || nc < 0 || nc >= dist[0].length || board[nr].charAt(nc) == 'D') {
                         nr -= dRow[d];
                         nc -= dCol[d];
                         break;
                     }
-                    
-                    nr += dRow[d];
-                    nc += dCol[d];
                 }
-                
-                if (distMap[nr][nc] == 0 && !map[nr][nc].equals("R")) {
-                    distMap[nr][nc] = distMap[now.r][now.c] + 1;
-                    queue.add(new Pair(nr, nc));
+                if (dist[nr][nc] == -1 && board[nr].charAt(nc) != 'D' && board[nr].charAt(nc) != 'R') {
+                    queue.offer(new Point(nr, nc));
+                    dist[nr][nc] = dist[now.r][now.c] + 1;
                 }
             }
-            
-        //     for (int[] a : distMap) {
-        //     for (int b : a) {
-        //         System.out.print(b + " ");
-        //     }
-        //     System.out.println();
-        // }
-        //     System.out.println("=====");
         }
-        
-        return -1;
-    }
-    
-    public boolean isOut(int nr, int nc) {
-        return nr < 0 || nr >= map.length || nc < 0 || nc >= map[0].length;
     }
 }
 
-class Pair {
+class Point {
     int r, c;
     
-    public Pair(int r, int c) {
+    public Point(int r, int c) {
         this.r = r;
         this.c = c;
     }
