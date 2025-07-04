6dfc08f1457cc9ca932100ef69fa28ca3ef3a513
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-07-04 11:22:52 +0900

    [level 2] Title: 할인 행사, Time: 34.82 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,17 +4,13 @@ class Solution {
     private static final int MEMBER_AUTH_DAY = 10;
     
     public int solution(String[] want, int[] number, String[] discount) {
-        Integer[] boxedNumber = Arrays.stream(number).boxed().toArray(Integer[]::new);
-        Arrays.sort(boxedNumber, Collections.reverseOrder());
-        int maxNumber = boxedNumber[0];
-        
         int answer = 0;
-        for (int i=0;i<=discount.length-MEMBER_AUTH_DAY;i++) { // 0~4
+        for (int i=0;i<=discount.length-MEMBER_AUTH_DAY;i++) {
             int start = i;
             int end = i+MEMBER_AUTH_DAY-1;
             int cnt = 0;
             int[] count = new int[number.length];
-            for (int j=start;j<=end;j++) { // 0~9
+            for (int j=start;j<=end;j++) {
                 for (int k=0;k<want.length;k++) {
                     if (want[k].equals(discount[j])) {
                         count[k]++;
@@ -24,7 +20,7 @@ class Solution {
             }
             boolean isSatisfied = true;
             for (int j=0;j<number.length;j++) {
-                if (number[j] > count[j]) {
+                if (number[j] != count[j]) {
                     isSatisfied = false;
                     break;
                 }
