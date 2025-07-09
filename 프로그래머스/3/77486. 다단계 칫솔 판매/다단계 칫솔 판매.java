329573e1bf3331e0b037d506d0c898b3428b1bfa
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-07-09 14:41:06 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 91.31 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,54 +1,45 @@
 import java.util.*;
 
 class Solution {
-    static Map<String, String> referrals = new HashMap<>();
-    static Map<String, Integer> benefits = new HashMap<>();
-    
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        // 추천인 map 초기화
+        Map<String, String> referrals = new HashMap<>();
         for (int i=0;i<enroll.length;i++) {
-            if (referral[i].equals("-")) {
-                referrals.put(enroll[i], "-");
-                continue;
-            }
             referrals.put(enroll[i], referral[i]);
         }
         
-        // 이익 map 초기화
-        for (int i=0;i<enroll.length;i++) {
-            benefits.put(enroll[i], 0);
+        for (int i=0;i<amount.length;i++) {
+            amount[i] *= 100;
         }
         
-        // 순환하면서 이익 map 채우기
+        Map<String, Integer> benefits = new HashMap<>();
         for (int i=0;i<seller.length;i++) {
-            int cost = amount[i] * 100;
-            if (cost / 10 > 0) {
-                benefits.put(seller[i], benefits.get(seller[i]) + cost - cost / 10);
-                dfs(referrals.get(seller[i]), cost / 10);
-            } else {
-                benefits.put(seller[i], benefits.get(seller[i]) + cost - cost / 10);
+            int distribution = amount[i] / 10;
+            int benefit = amount[i] - distribution;
+            benefits.put(seller[i], benefits.getOrDefault(seller[i], 0) + benefit);
+            
+            if (referrals.get(seller[i]).equals("-")) {
+                continue;
             }
             
+            dfs(referrals, benefits, referrals.get(seller[i]), distribution);
         }
         
         int[] answer = new int[enroll.length];
         for (int i=0;i<enroll.length;i++) {
-            answer[i] = benefits.get(enroll[i]);
+            answer[i] = benefits.getOrDefault(enroll[i], 0);
         }
-        
         return answer;
     }
     
-    public void dfs(String referral, int amount) {
-        if (referral.equals("-")) {
-            return;
-        }
+    public void dfs(Map<String, String> referrals, Map<String, Integer> benefits, String now, int amount) {
+        int distribution = amount / 10;
+        int benefit = amount - distribution;
+        benefits.put(now, benefits.getOrDefault(now, 0) + benefit);
         
-        if (amount / 10 > 0) {
-            benefits.put(referral, benefits.get(referral) + amount - amount / 10);
-            dfs(referrals.get(referral), amount / 10);
+        if (referrals.get(now).equals("-") || distribution < 1) {
+            return;
         } else {
-            benefits.put(referral, benefits.get(referral) + amount - amount / 10);
+            dfs(referrals, benefits, referrals.get(now), distribution);
         }
     }
 }
