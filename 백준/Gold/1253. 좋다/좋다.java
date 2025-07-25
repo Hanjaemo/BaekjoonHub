e7a0b71c22b68074a19ac257421979f63ea61e80
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2025-07-25 11:34:31 +0900

    [Gold IV] Title: 좋다, Time: 144 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -6,50 +6,43 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 수의 개수(n) 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        // 배열 생성 및 정렬
         int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
         Arrays.sort(arr);
 
-        // for (k를 0부터 n번 반복)
-            // while (i < j)
-                // if (arr[i] + arr[j] > arr[k]) : j 감소
-                // if (arr[i] + arr[j] < arr[k]) : i 증가
-                // if (arr[i] + arr[j] == arr[k]) : count 증가, i 증가, j 감소, 종료
-        int count = 0;
-        for (int k = 0; k < n; k++) {
-            int i = 0;
-            int j = n - 1;
-            while (i < j) {
-                 if (arr[i] + arr[j] > arr[k]) {
-                     j--;
-                     continue;
-                 }
-                 if (arr[i] + arr[j] < arr[k]) {
-                     i++;
-                     continue;
-                 }
-                 if (arr[i] + arr[j] == arr[k]) {
-                     if (i != k && j != k) {
-                         count++;
-                         break;
-                     } else if (i == k) {
-                         i++;
-                     } else {
-                         j--;
-                     }
-                 }
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int l = 0;
+            int r = n - 1;
+            while (l < r) {
+                int sum = arr[l] + arr[r];
+                int target = arr[i];
+                if (sum == target) {
+                    if (l != i && r != i) {
+                        answer++;
+                        break;
+                    } else if (l == i) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                    continue;
+                }
+                if (sum < target) {
+                    l++;
+                } else {
+                    r--;
+                }
             }
         }
 
-        // count 출력
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
